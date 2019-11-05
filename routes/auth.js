a6e1509a22573d301be3c5e7ae8746ee3fa507c3
wi <letuthptnguyendueduvn@gmail.com>
2019-11-06 02:41:41 +0700

    refactor

--- routes/auth.js
+++ routes/auth.js
@@ -2,14 +2,14 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 
-router.get("/facebook", passport.authenticate("facebook"));
+// router.get("/facebook", passport.authenticate("facebook"));
 
-router.get(
-  "/facebook/callback",
-  passport.authenticate("facebook", {
-    successRedirect: "/",
-    failureRedirect: "/login"
-  })
-);
+// router.get(
+//   "/facebook/callback",
+//   passport.authenticate("facebook", {
+//     successRedirect: "/",
+//     failureRedirect: "/login"
+//   })
+// );
 
 module.exports = router;
--- routes/game.js
+++ routes/game.js
@@ -29,6 +29,21 @@ router.get("/:gid", passport.authenticate("jwt"), function(req, res) {
     .catch(err => console.log("letu err get game", err));
 });
 
+router.post("/:gid/jump", passport.authenticate("jwt"), function(req, res) {
+  Games.findOne({ _id: req.params.gid })
+    .then(data => {
+      Games.updateOne(
+        { _id: req.params.gid },
+        { $set: { history: data.history.splice(0, req.body.step + 1) } }
+      )
+        .then(data2 => {
+          return res.status(200).json(data2);
+        })
+        .catch(err => console.log("letu err update user", err));
+    })
+    .catch(err => console.log("letu err get game", err));
+});
+
 router.post("/:gid", passport.authenticate("jwt"), function(req, res) {
   Games.updateOne(
     { _id: req.params.gid },
